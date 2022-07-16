277f2d1f64caa54b4066bb48cd000de7be7c0290
Daniel Chicchon <danielchicchon@gmail.com>
2022-07-16 15:09:51 -0700

    update config and shell script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run gh-pages
 
 # navigate into the build output directory
-cd dist
+cd browser-build
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:dchicchon/notepad.git main:gh-pages
 
 cd -
