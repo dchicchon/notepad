f9e9bbfcd79bd3ecf420ef3779993a6f43f43e19
Daniel Chicchon <danielchicchon@gmail.com>
2022-07-20 11:56:16 -0700

    remove unused packages

--- src/utils/hotkeyMap.js
+++
@@ -1,4 +0,0 @@
-export const OPEN_FILE_HOTKEY = 'ctrl+o, command+o'
-export const SAVE_FILE_HOTKEY = 'ctrl+s, command+s'
-export const INCREASE_FONT = 'ctrl+=, command+='
-export const DECREASE_FONT = 'ctrl+-, command+-'
